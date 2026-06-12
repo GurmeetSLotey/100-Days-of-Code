73d2ac49332d327606535ba344d889cd2bc18d19
GurmeetSLotey <38222282+GurmeetSLotey@users.noreply.github.com>
2026-06-11 23:03:26 -0700

    Learned about /n, input function and variables

--- Day-1/main.py
+++ Day-1/main.py
@@ -1,2 +1,13 @@
+
 print("Hello World!")
 print("Start of a new journey")
+
+print('\\n allows us to print in multiple lines')
+
+print('Hello World \\nStart of a new Jounrey will print on two lines')
+
+# print("Hello " + input("What is your name? "))
+
+users_name = input("What is your name? ")
+
+print("Hello " + users_name)
