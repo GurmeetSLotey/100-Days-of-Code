8ff1d1af7dab1a9d3d10797b7e0ff5064f547570
GurmeetSLotey <38222282+GurmeetSLotey@users.noreply.github.com>
2026-06-26 14:10:58 -0700

    initiating Day 14 and 15

---
+++ Day_14/higher_lower.py
@@ -0,0 +1,9 @@
+"""
+User is given two options to choose from in this case it'll be
+two people. The user will select option A or B for whom they believe
+has a higher count of followers on social media. 
+If the user guesses correctly their score will increase by one.
+If the user guesses incorrectly it'll be game over and their score will be displayed
+"""
+
+# TODO 
