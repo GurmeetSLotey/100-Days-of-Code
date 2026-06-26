8d18fe0db0aa9e9f358b327fb543285254984dee
GurmeetSLotey <38222282+GurmeetSLotey@users.noreply.github.com>
2026-06-26 14:21:27 -0700

    setup for Day 14 and 15

--- Day_15/coffee_machine.py
+++ Day_15/coffee_machine.py
@@ -1,20 +1,2 @@
-"""
-Coffee Machine
-User will have an option of 3 different types of coffees
-Espresso:
-    50ml Water
-    18g Coffee
-    $1.50
-
-Latte:
-    200ml Water
-    24g Coffee
-    150ml Milk
-    $2.50
-
-Cappuccino:
-    250ml Water
-    24g Coffee
-    100ml Milk
-    $3.00
-"""
+from packages.ingredients import menu
+from packages.ingredients import resources
