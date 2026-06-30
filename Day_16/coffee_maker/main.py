b9048758583f08ff641c579523700b1a91163c18
GurmeetSLotey <38222282+GurmeetSLotey@users.noreply.github.com>
2026-06-29 19:43:17 -0700

    coffee maker using OOP

--- Day_16/coffee_maker/main.py
+++ Day_16/coffee_maker/main.py
@@ -1,3 +1,23 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
 from money_machine import MoneyMachine
+
+coffee_maker = CoffeeMaker()
+menu = Menu()
+money_machine = MoneyMachine()
+
+while True:
+    user_order = input(f"What would you like {menu.get_items()}? ").lower()
+
+    if user_order == 'off':
+        break
+
+    elif user_order == "print":
+        coffee_maker.report()
+        money_machine.report()
+
+    elif menu.find_drink(user_order) and coffee_maker.is_resource_sufficient(menu.find_drink(user_order)):
+        while not money_machine.make_payment(menu.find_drink(user_order).cost):
+            pass
+        coffee_maker.make_coffee(menu.find_drink(user_order))
+        
